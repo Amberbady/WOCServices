d43143ff06b7176f92636a890b7cb291c4b2eb9e
廖燊 <13690039192@163.com>
2023-05-17 17:35:40 +0800

    新添加tool文件

--- WOCServices/Classes/WOCServicesTool.h
+++ WOCServices/Classes/WOCServicesTool.h
@@ -1,15 +1,16 @@
 //
-//  WOCProtocolRouter.h
-//  BeeHive
+//  WOCServicesTool.h
+//  WOCServices
 //
 //  Created by 廖燊 on 2023/5/17.
 //
 
 #import <Foundation/Foundation.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol WOCProtocolRouter <NSObject, BHServiceProtocol>
-- (void)doSome;
+@interface WOCServicesTool : NSObject
+
 @end
 
 NS_ASSUME_NONNULL_END
---
+++ WOCServices/Classes/WOCServicesTool.m
@@ -0,0 +1,12 @@
+//
+//  WOCServicesTool.m
+//  WOCServices
+//
+//  Created by 廖燊 on 2023/5/17.
+//
+
+#import "WOCServicesTool.h"
+
+@implementation WOCServicesTool
+
+@end
