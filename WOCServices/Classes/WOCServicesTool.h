9c4f013b09cbe904ef9a3cc8651c4d7e0b8be0d4
廖燊 <13690039192@163.com>
2023-05-17 17:38:11 +0800

    测试

--- WOCServices/Classes/WOCServicesTool.h
+++ WOCServices/Classes/WOCServicesTool.h
@@ -6,11 +6,20 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "BeeHive.h"
 NS_ASSUME_NONNULL_BEGIN
 
 @interface WOCServicesTool : NSObject
+/**
+ 检查services协议方法，存在即回调completion，不存在则报错
 
+ @param p 协议
+ @param sel 方法
+ @param completion 确认协议和方法存在时回调
+ @param error 错误回调，可为nil
+ */
++ (void)servicesProtocol:(Protocol *)p action:(SEL)sel completion:(void (^)(id service))completion error:(void(^)(NSString *errorMsg))error;
 @end
 
 NS_ASSUME_NONNULL_END
+
--- WOCServices/Classes/WOCServicesTool.m
+++ WOCServices/Classes/WOCServicesTool.m
@@ -8,5 +8,27 @@
 #import "WOCServicesTool.h"
 
 @implementation WOCServicesTool
-
+/**
+ 检查services协议方法，存在即回调completion，不存在则报错
+ 
+ @param p 协议
+ @param sel 方法
+ @param completion 确认协议和方法存在时回调
+ @param error 错误回调，可为nil
+ */
++ (void)servicesProtocol:(Protocol *)p action:(SEL)sel completion:(void (^)(id service))completion error:(nonnull void (^)(NSString * _Nonnull))error
+{
+    id aService = [[BHServiceManager sharedManager] createService:p];
+    if ([aService respondsToSelector:sel]) {
+        completion(aService);
+    } else {
+        // 错误信息
+        NSString *msg = [NSString stringWithFormat:@"协议：%@ 没有实现方法：%@", NSStringFromProtocol(p), NSStringFromSelector(sel)];
+        // 错误回调
+        if (error) {
+            error(msg);
+        }
+    }
+}
 @end
+
